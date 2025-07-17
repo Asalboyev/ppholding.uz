57f794cf7f4026280a2a7abd78d1d3b1511e6162
Axror <100289253+Asalboyev@users.noreply.github.com>
2025-07-17 15:50:42 +0500

    fix

--- files/app/Http/Controllers/WebController.php
+++ files/app/Http/Controllers/WebController.php
@@ -89,8 +89,8 @@ class WebController extends Controller
 
         // Mahsulotlar - 'order' ustunini DECIMAL sifatida to'g'ri tartiblash
         $products = Product::where('catalog_id', $catalog->id)
-            ->orderByRaw('CAST(SUBSTRING_INDEX(`order`, ".", 1) AS UNSIGNED) ASC') // Category id bo‘yicha
-            ->orderByRaw('CAST(SUBSTRING_INDEX(`order`, ".", -1) AS UNSIGNED) ASC') // Category ichidagi tartib
+            ->orderByRaw('CAST(SUBSTRING_INDEX(`order`, ".", 1) AS UNSIGNED) ASC')  // category part
+            ->orderByRaw('CAST(SUBSTRING_INDEX(`order`, ".", -1) AS DECIMAL(10,3)) ASC') // product order part
             ->paginate(9);
 
         // Umumiy mahsulotlar soni
